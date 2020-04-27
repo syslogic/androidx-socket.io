f44a3d1bdb724bb4fafc4a155e93a05331a18571
Martin Zeitler <android@syslogic.io>
2020-04-27 21:23:50 +0200

    local debug URL added.

--- mobile/src/main/java/io/syslogic/socketio/Constants.java
+++ mobile/src/main/java/io/syslogic/socketio/Constants.java
@@ -2,6 +2,10 @@ package io.syslogic.socketio;
 
 class Constants {
 
-    /** currently down; causes io.socket.engineio.client.EngineIOException: xhr poll error */
-    static String CHAT_SERVER_URL = "https://socket-io-chat.now.sh/";
+    private static String DEBUG_URL ="http://192.168.1.1:3000/";
+
+    private static String LIVE_URL = "https://.../";
+
+    /** CHAT_SERVER_URL may cause io.socket.engineio.client.EngineIOException: xhr poll error when down */
+    static String CHAT_SERVER_URL = BuildConfig.DEBUG ? DEBUG_URL : LIVE_URL;
 }
