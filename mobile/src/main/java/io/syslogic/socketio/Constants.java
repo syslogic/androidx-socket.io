b7c81efcfe8555bb8641fc489967cac226c36a62
Martin Zeitler <android@syslogic.io>
2020-04-27 15:13:00 +0200

    base URL made a constant.

---
+++ mobile/src/main/java/io/syslogic/socketio/Constants.java
@@ -0,0 +1,7 @@
+package io.syslogic.socketio;
+
+class Constants {
+
+    /** currently down; causes io.socket.engineio.client.EngineIOException: xhr poll error */
+    static String CHAT_SERVER_URL = "https://socket-io-chat.now.sh/";
+}
