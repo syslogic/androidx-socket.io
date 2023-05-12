755d54c624900a60d930a0d6d23cce62cd756edb
Martin Zeitler <martin@syslogic.io>
2023-05-12 18:26:26 +0200

    AGP 8.0.1

--- mobile/src/main/java/io/syslogic/socketio/Constants.java
+++ mobile/src/main/java/io/syslogic/socketio/Constants.java
@@ -2,9 +2,9 @@ package io.syslogic.socketio;
 
 class Constants {
 
-    private static String DEBUG_URL ="http://192.168.2.96:3000/";
+    private static final String DEBUG_URL ="http://192.168.2.96:3000/";
 
-    private static String LIVE_URL = "https://.../";
+    private static final String LIVE_URL = "https://.../";
 
     /** CHAT_SERVER_URL may cause io.socket.engineio.client.EngineIOException: xhr poll error when down */
     static String CHAT_SERVER_URL = BuildConfig.DEBUG ? DEBUG_URL : LIVE_URL;
