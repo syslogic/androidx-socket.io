13a1c2bc4afd1ab886c249bf43fe4094b3897ddb
Martin Zeitler <martin@syslogic.io>
2024-03-23 00:14:10 +0100

    AGP 8.3.1

--- mobile/src/androidTest/java/io/syslogic/socketio/ExampleInstrumentedTest.java
+++ mobile/src/androidTest/java/io/syslogic/socketio/ExampleInstrumentedTest.java
@@ -1,14 +1,14 @@
 package io.syslogic.socketio;
 
+import static junit.framework.TestCase.assertEquals;
+
 import android.content.Context;
 
-import androidx.test.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static junit.framework.Assert.assertEquals;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -17,10 +17,10 @@ import static junit.framework.Assert.assertEquals;
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() throws Exception {
+    public void useAppContext() {
 
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("io.syslogic.socketio", appContext.getPackageName());
     }
 }
