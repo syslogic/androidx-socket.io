b84a8b9153c76d5f6f03d116d7b4df58aebb8ca0
Martin Zeitler <martin@syslogic.io>
2025-03-26 02:05:07 +0100

    instancing the server in app.js

---
+++ server/modules/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const port = process.env.PORT || 3000;
+const { Server } = require("socket.io");
+
+const app = express();
+app.use(express.static(__dirname.replace("modules", "public")));
+const server = require("http").createServer(app);
+
+server.listen(port, () => {
+    console.log("Server listening at port %d", port);
+});
+
+const io = new Server(server, {
+    transports: ["polling", "websocket"],
+    path: "/socket.io"
+});
+
+io.on('connection', (socket) => {
+    socket.join('default');
+    require("./connection")(io, socket);
+    require("./message")(io, socket);
+    require("./user")(io, socket);
+    require("./typing")(io, socket);
+});
+
+module.exports = io;
