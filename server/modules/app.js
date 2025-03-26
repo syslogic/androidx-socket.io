6004ee5ed57cb4a8855b538410d741b84d95b4ce
Martin Zeitler <martin@syslogic.io>
2025-03-26 02:12:05 +0100

    instancing the server in app.js

--- server/modules/app.js
+++ server/modules/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const port = process.env.PORT || 3000;
 const { Server } = require("socket.io");
+const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.static(__dirname.replace("modules", "public")));
